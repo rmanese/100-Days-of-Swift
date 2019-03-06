381c4c68572433ed57d98ad391a7193bab062c19
rman <rjmanese@gmail.com>
2019-03-06 10:37:22 -0800

    load json data and present it on tableview

--- Project6/Project6/Petition.swift
+++ Project6/Project6/Petition.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
--- Project6/Project6/Petitions.swift
+++ Project6/Project6/Petitions.swift
@@ -7,3 +7,7 @@
 //
 
 import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
