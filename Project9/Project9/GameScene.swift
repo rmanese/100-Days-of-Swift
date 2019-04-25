50c1b5f7ff4d5f1bb9cdeb6681b4348b89afd3cc
rman <rjmanese@gmail.com>
2019-04-25 09:55:13 -0700

    spritekit day

---
+++ Project9/Project9/GameScene.swift
@@ -0,0 +1,33 @@
+//
+//  GameScene.swift
+//  Project9
+//
+//  Created by Roberto Manese III on 4/24/19.
+//  Copyright © 2019 jawnyawn. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        let box = SKSpriteNode(color: .green, size: CGSize(width: 64, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        box.position = location
+        addChild(box)
+    }
+}
