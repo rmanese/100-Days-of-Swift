2bb049c5845dec4d589a669b5b26664b28d5d39f
rman <rjmanese@gmail.com>
2019-04-25 16:53:35 -0700

    implement collision detection between two objects, and score system. pretty cools

--- Project9/Project9/GameScene.swift
+++ Project9/Project9/GameScene.swift
@@ -9,7 +9,14 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
 
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
@@ -18,13 +25,25 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
 
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
+
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+
         makeBouncer(at: CGPoint(x: 0, y: 0))
         makeBouncer(at: CGPoint(x: 256, y: 0))
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
-
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,7 +53,9 @@ class GameScene: SKScene {
         let ball = SKSpriteNode(imageNamed: "ballRed")
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
         ball.physicsBody?.restitution = 1
+        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
         ball.position = location
+        ball.name = "ball"
         addChild(ball)
     }
 
@@ -45,4 +66,57 @@ class GameScene: SKScene {
         bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
     }
+
+    private func makeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
+        }
+
+        slotBase.position = position
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
+
+        slotGlow.position = position
+
+        let spin = SKAction.rotate(byAngle: .pi / 2, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
+
+        addChild(slotBase)
+        addChild(slotGlow)
+    }
+
+    func collision(between ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+            score += 1
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+            score -= 1
+        }
+    }
+
+    func destroy(ball: SKNode) {
+        ball.removeFromParent()
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let bodyA = contact.bodyA.node else { return }
+        guard let bodyB = contact.bodyB.node else { return }
+        if bodyA.name == "ball" {
+            collision(between: bodyA, object: bodyB)
+        } else if bodyB.name == "ball" {
+            collision(between: bodyB, object: bodyA)
+        }
+    }
+    
 }
