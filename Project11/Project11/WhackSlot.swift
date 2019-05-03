c2ac9bc471ee99c8d3599eb973ca083d214a641f
rman <rjmanese@gmail.com>
2019-05-03 14:08:19 -0700

    playable whack-a-penguin

--- Project11/Project11/WhackSlot.swift
+++ Project11/Project11/WhackSlot.swift
@@ -12,6 +12,9 @@ import UIKit
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
 
+    var isVisible = false
+    var isHit = false
+
     func configure(at point: CGPoint) {
         position = point
 
@@ -23,11 +26,53 @@ class WhackSlot: SKNode {
         cropNode.zPosition = 1
         cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
 
-        let charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
         charNode.position = CGPoint(x: 0, y: -90)
         charNode.name = "character"
         cropNode.addChild(charNode)
 
         addChild(cropNode)
+    }
+
+    func show(hideTime: Double) {
+        if isVisible { return }
+        charNode.xScale = 1
+        charNode.yScale = 1
+
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [weak self] in
+            self?.hide()
+        }
+    }
+
+    func hide() {
+        if !isVisible { return }
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+
+    func hit() {
+        isHit = true
+
+        let delay = SKAction.wait(forDuration: 0.25)
+        let move = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run {
+            [weak self] in
+            self?.isVisible = false
+        }
+        let sequence = SKAction.sequence([delay, move, notVisible])
+        charNode.run(sequence)
     }
 }
